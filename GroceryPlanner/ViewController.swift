42d253aae952b330e6c2a24963f99871a5eb0c12
prathyusha makam <prathyusha44m@gmail.com>
2016-11-20 21:00:52 -0800

    good

--- GroceryPlanner/ViewController.swift
+++ GroceryPlanner/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("checking")
+        print("done checking")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
