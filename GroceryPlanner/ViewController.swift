7b76b0ea0131c52e974c1ff7382b7aac8215ba07
prathyusha makam <prathyusha44m@gmail.com>
2016-12-08 18:53:01 -0800

    MenuBar is completed

--- GroceryPlanner/ViewController.swift
+++ GroceryPlanner/ViewController.swift
@@ -9,16 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var menuView: UIView!
+    var menuShowing = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Shawdow effect for menu
+        menuView.layer.shadowOpacity = 1
+        menuView.layer.shadowRadius = 6
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    
+    // Open menu on clicking menu button by sliding
+    @IBAction func openMenu(_ sender: AnyObject) {
+        
+        // Creating the sliding effect for the menuBar
+        if(menuShowing){
+            leadingConstraint.constant = -215
+        } else {
+            leadingConstraint.constant = 0
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        menuShowing = !menuShowing
     }
-
-
+    
 }
 
